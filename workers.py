2622430aa01706063e5d183f47b2e0e9b6e9e4f0
Zkh-dot <kernzahar@gmail.com>
2024-01-07 20:18:08 +0300

    phone support

--- workers.py
+++ workers.py
@@ -5,6 +5,8 @@ from clasters import proxmox_claster, hadoop_claster
 from vm import vm
 from typing import Union
 from simpy import Environment
+from state import state
+from random import randint
 
 class common_worker(ABC):
     @abstractclassmethod
@@ -14,6 +16,14 @@ class common_worker(ABC):
         self.__time_to_examine = config["TIME_TO_EXAMINE"]["BASE"]
         self.__can_fix = []
         self.__env = env
+        self.__state = state([])
+        
+    @property
+    def state(self):
+        return self.__state.state
+    
+    def change_state(self):
+        self.__state.change_state()
         
     def fix(self, resource: Union(server, proxmox_claster, hadoop_claster, vm)):
         self.__env.timeout(self.__time_go_for_work)
@@ -59,5 +69,45 @@ class big_boy(common_worker):
         self.__can_fix = [hadoop_claster, proxmox_claster, vm]
         
 class phone_support:
-    def __init__(self):
-        self.
+    def __init__(self, env: Environment):
+        __devopses = [devops(env) for x in config["WORKER_AMOUNT"]["DEVOPS"]]
+        __hadoop_enjs = [hadoop_enj(env) for x in config["WORKER_AMOUNT"]["HADOOP_ENJ"]]
+        __proxmox_enjs = [proxmox_enj(env) for x in config["WORKER_AMOUNT"]["PROXMOX_ENJ"]]
+        __big_boys = [big_boy(env) for x in config["WORKER_AMOUNT"]["BIG_BOYS"]]
+        self.__workers = {}
+        self.__workers["devops"] = __devopses
+        self.__workers["hadoop_enj"] = __hadoop_enjs
+        self.__workers["proxmox_enj"] = __proxmox_enjs
+        self.__workers["big_boys"] = __big_boys
+        self.__env = env
+        
+    def search_free_worker(self, worker_group: list):
+        for worker in worker_group:
+            if not worker.state:
+                return worker
+        return None
+        
+    def deal_with_problem(self, problem: Union(server, proxmox_claster, hadoop_claster, vm)):
+        for group in self.__workers:
+            if config["RECOGNIZE_PROBLEM_CHANCE"] < randint(0, 100):
+                if type(problem) is server:
+                    group = "big_boys"
+                elif type(problem) is proxmox_claster:
+                    group = "proxmox_enj"
+                elif type(problem) is hadoop_claster:
+                    group = "hadoop_enj"
+                elif type(problem) is vm:
+                    group = "devops"
+                else:
+                    raise Exception("unknown type of problem!")
+            worker = None
+            while worker is None:
+                worker = self.search_free_worker(self.__workers[group])
+                if worker:
+                    break
+                self.__env.timeout(config["TIME_TO_WAIT_FOR_WORKER"])
+                
+            if worker.fix(problem):
+                return True
+            
+        raise Exception("no one can fix it!")
